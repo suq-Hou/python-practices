b3827024feb832583c5c189fc513be1f71a56742
SuqinHou <suqinhou@gmail.com>
2020-09-12 20:37:16 -0400

    bunch of changes

--- RedditContentAggregator/bin/TopNewsSender.py
+++ RedditContentAggregator/bin/TopNewsSender.py
@@ -1,6 +1,7 @@
 import argparse
 from RedditContentAggregator.tasks.RedditScraper import RedditScraper
 from RedditContentAggregator.tasks.EmailSender import EmailSender
+from RedditContentAggregator.tasks.MySQLStore import MySQLStore
 
 Parser = argparse.ArgumentParser()
 
@@ -16,32 +17,43 @@ Parser.add_argument("--address", nargs='+', help='email address of the recipient
 
 ## optional args
 # total pages you'd like to scrape, default is 3
-Parser.add_argument("-pages", "--Total pages scrape", help="total pages you'd like to scrape")
+Parser.add_argument("-pages", help="total pages you'd like to scrape")
 
 # top N results stored, default is 10
-Parser.add_argument("-results", "--Top results", help="top results you'd like to scrape")
+Parser.add_argument("-results", help="top results you'd like to scrape")
+
+# whether creating new mysql table or not
+Parser.add_argument('-new_table', help="whether create new subreddit table in mysql or not")
 
 args = Parser.parse_args()
+print(args)
 
 if __name__ == "__main__":
     # default: scrape the top 3 pages in that subreddit, return the top 10 results with most upvotes
-    try:
-        args.pages
-    except:
-        args.pages = 10
+    if args.pages is None:
+        args.pages = 3
 
-    try:
-        args.results
-    except:
-        args.results = 3
+    if args.results is None:
+        args.results = 10
 
-    reddit = RedditScraper(subreddit_name=args.subreddit, total_pages_scrape=3, total_posts=10)
-    results = reddit.scraper() # store scraped results
+    if args.new_table is None:
+        args.new_table = False
 
-    # compose email
-    email = EmailSender(emailRecipients=args.address,
-                        emailBody=results,
-                        emailSubject=f'Top {args.pages} results from /reddit/{args.subreddit}/'
-                        )
+    reddit = RedditScraper(subreddit_name=args.subreddit,
+                           total_pages_scrape=args.pages,
+                           total_posts=args.results)
+    results_df, results_html = reddit.scraper() # store scraped results
 
-    email.sendEmail()
+    # create mysql object that stores pulled results
+    mysql = MySQLStore(subreddit=args.subreddit, crawled_posts=results_df, create_new_table=args.new_table)
+
+    if mysql.run(): # new posts have less than 80% overlap with the latest posts in the database
+        # compose email
+        email = EmailSender(emailRecipients=args.address,
+                            emailBody=results_html,
+                            emailSubject=f'Top {args.results} results from /reddit/{args.subreddit}/'
+                            )
+
+        email.sendEmail()
+    else:
+        print("No new posts today.")
